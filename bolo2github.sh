ac195b37bc128d8c9e31c0d6c5b4159678dba38e
Kolen <christian.kolen@gmail.com>
2018-10-05 18:11:46 -0700

    bolo2github.sh: minor change in error message

--- bolo2github.sh
+++ bolo2github.sh
@@ -41,4 +41,4 @@ done
 
 nameWOExt="${name%.*}"
 cd "$input/$name"
-git push --mirror "$output/$nameWOExt.git" || (>&2 printf '%s\n' 'Cannot mirror' "$name" )
+git push --mirror "$output/$nameWOExt.git" || (>&2 printf '%s\n' "[FAILED] cannot mirror $name" )
